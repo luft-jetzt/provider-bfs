9fc94f3d4473b390cf52b330ac2e359ca7702598
Malte Hübner <maltehuebner@gmx.org>
2024-06-24 15:55:41 +0200

    Improve converter.

--- src/Bfs/Coordinate/Converter.php
+++ src/Bfs/Coordinate/Converter.php
@@ -11,15 +11,15 @@ class Converter
 
     public static function convert(string $coordinateString): Coord
     {
-        preg_match_all('/(\d+)°(\d+)\'(\d+)"/', $coordinateString, $matches, PREG_SET_ORDER);
+        preg_match_all('/(\d+)°(\d+)\'((\d+)")?/', $coordinateString, $matches, PREG_SET_ORDER);
 
         $latDeg = (float) $matches[0][1];
         $latMin = (float) $matches[0][2];
-        $latSec = (float) $matches[0][3];
+        $latSec = array_key_exists(3, $matches[0]) ? (float) $matches[0][3] : 0;
 
         $lonDeg = (float) $matches[1][1];
         $lonMin = (float) $matches[1][2];
-        $lonSec = (float) $matches[1][3];
+        $lonSec = array_key_exists(3, $matches[1]) ? (float) $matches[1][3] : 0;
 
         $decimalLatitude = $latDeg + ($latMin / 60) + ($latSec / 3600);
         $decimalLongitude = $lonDeg + ($lonMin / 60) + ($lonSec / 3600);
