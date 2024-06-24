3915c47bbd353ceb4f4cc272b4f2d5b988204be4
Malte Hübner <maltehuebner@gmx.org>
2024-06-24 17:12:27 +0200

    Resolve all dependencies.

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Caldera\LuftApiBundle\LuftApiBundle::class => ['all' => true],
 ];
