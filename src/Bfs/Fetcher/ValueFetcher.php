541fb94aa0b9d569d829f51d5de6951043fe6bdb
Malte Hübner <maltehuebner@gmx.org>
2024-06-27 17:49:49 +0200

    Improve ValueFetcher and add test.

--- src/Bfs/Fetcher/ValueFetcher.php
+++ src/Bfs/Fetcher/ValueFetcher.php
@@ -2,10 +2,14 @@
 
 namespace App\Bfs\Fetcher;
 
+use App\Bfs\Graph\CurrentDateTime;
+use App\Bfs\Graph\HourRange;
+use App\Bfs\Graph\Maintenance;
 use App\Bfs\Graph\Point;
 use App\Bfs\Graph\StepSize;
 use App\Bfs\Website\StationModel;
 use Caldera\LuftModel\Model\Value;
+use Carbon\Carbon;
 use Imagine\Gd\Imagine;
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -13,10 +17,32 @@ class ValueFetcher implements ValueFetcherInterface
 {
     public function fromStation(StationModel $stationModel): Value
     {
-        $imagine = new Imagine();
-
         $binaryImagecontent = $this->loadImageContent($stationModel->getCurrentImageUrl());
+
+        $imagine = new Imagine();
         $image = $imagine->load($binaryImagecontent);
+
+        $dateTime = CurrentDateTime::calculate($image);
+
+        $value = new Value();
+        $value
+            ->setStationCode($stationModel->getStationCode())
+            ->setPollutant('UV')
+            ->setDateTime($dateTime)
+            ->setValue(0)
+        ;
+
+        if (Maintenance::isMaintenance($image)) {
+            return $value;
+        }
+
+        $hourRange = HourRange::calculate($image);
+
+        $now = new Carbon();
+
+        if ($now->format('H') < $hourRange->getStartHour() || $now->format('H') >= $hourRange->getEndHour()) {
+            return $value;
+        }
 
         $stepSize = StepSize::detectStepSize($image);
 
@@ -24,24 +50,20 @@ class ValueFetcher implements ValueFetcherInterface
 
         $y = 385 - $currentPoint->getY() + 50;
 
-        $uvIndex = round((($y / $stepSize) + 1) / 2, 2);
+        $uvIndex = round((($y / $stepSize) + 1) / 2, 1);
 
-        $value = new Value();
-        $value
-            ->setStationCode($stationModel->getStationCode())
-            ->setPollutant('UV')
-            ->setDateTime(new \DateTime())
-            ->setValue($uvIndex)
-        ;
-
-        return $value;
+        return $value->setValue($uvIndex);
     }
 
     protected function loadImageContent(string $url): string
     {
-        $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', $url);
+        if (str_starts_with($url, 'https://')) {
+            $httpClient = HttpClient::create();
+            $response = $httpClient->request('GET', $url);
 
-        return $response->getContent();
+            return $response->getContent();
+        }
+
+        return file_get_contents($url);
     }
 }
---
+++ tests/Unit/Fetcher/ValueFetcherTest.php
@@ -0,0 +1,117 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Unit\Fetcher;
+
+use App\Bfs\Fetcher\ValueFetcher;
+use App\Bfs\Website\StationModel;
+use Caldera\LuftModel\Model\Value;
+use Carbon\Carbon;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class ValueFetcherTest extends TestCase
+{
+    #[DataProvider('graphFilenameProvider')]
+    public function testValueFetcher(string $currentImageUrl, float $expectedUvIndex, Carbon $expectedDateTime, Carbon $currentDateTime): void
+    {
+        Carbon::setTestNow($currentDateTime);
+
+        $expectedValue = new Value();
+        $expectedValue
+            ->setPollutant('UV')
+            ->setStationCode('TEST123')
+            ->setValue($expectedUvIndex)
+            ->setDateTime($expectedDateTime)
+        ;
+
+        $valueFetcher = new ValueFetcher();
+
+        $stationModel = new StationModel();
+        $stationModel
+            ->setCurrentImageUrl($currentImageUrl)
+            ->setStationCode('TEST123')
+        ;
+
+        $value = $valueFetcher->fromStation($stationModel);
+
+        $this->assertEquals($expectedValue, $value);
+    }
+
+    public static function graphFilenameProvider(): array
+    {
+        return [
+            [
+                __DIR__ . '/../../graph/boesel.png',
+                2.7,
+                (new Carbon())->setTime(9, 3),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/friedrichshafen1.png',
+                0.9,
+                (new Carbon())->setTime(10, 0),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/friedrichshafen2.png',
+                0,
+                (new Carbon())->setTime(20, 58),
+                (new Carbon())->setTime(21, 5),
+            ],
+            [
+                __DIR__ . '/../../graph/hamburg1.png',
+                3,
+                (new Carbon())->setTime(17, 50),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/hamburg2.png',
+                4.2,
+                (new Carbon())->setTime(9, 54),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/lueneburg1.png',
+                0.6,
+                (new Carbon())->setTime(18, 10),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/lueneburg2.png',
+                5.3,
+                (new Carbon())->setTime(14, 9),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/lueneburg3.png',
+                4.6,
+                (new Carbon())->setTime(15, 9),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/lueneburg4.png',
+                5.4,
+                (new Carbon())->setTime(15, 0),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/sanktaugustin.png',
+                0,
+                (new Carbon())->setTime(5, 56),
+                (new Carbon())->setTime(5, 56),
+            ],
+            [
+                __DIR__ . '/../../graph/schneefernhaus1.png',
+                4.4,
+                (new Carbon())->setTime(9, 50),
+                (new Carbon())->setTime(9, 3),
+            ],
+            [
+                __DIR__ . '/../../graph/schneefernhaus2.png',
+                1.3,
+                (new Carbon())->setTime(15, 10),
+                (new Carbon())->setTime(9, 3),
+            ],
+        ];
+    }
+}
