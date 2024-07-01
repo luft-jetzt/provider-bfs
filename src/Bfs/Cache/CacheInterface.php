c356865ab75e76f94c94a48a3e6f871ad5134a82
Malte Hübner <maltehuebner@gmx.org>
2024-07-01 22:36:56 +0200

    Adjust cache ttl.

--- src/Bfs/Cache/CacheInterface.php
+++ src/Bfs/Cache/CacheInterface.php
@@ -6,6 +6,6 @@ interface CacheInterface
 {
     public const string CACHE_NAMESPACE = 'station_cache';
     public const string CACHE_KEY = 'station_list';
-    public const int CACHE_TTL = 60 * 60;
+    public const int CACHE_TTL = 60 * 60 * 24;
     public const string CACHE_DIRECTORY = __DIR__.'/../../../var/cache/';
 }
