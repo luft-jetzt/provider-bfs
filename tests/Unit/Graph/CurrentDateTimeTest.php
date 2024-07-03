cf871953b2e56c82f9820c68bfc173b14754e5ca
Malte Hübner <maltehuebner@gmx.org>
2024-07-03 12:32:06 +0200

    Fix datetimetest.

--- tests/Unit/Graph/CurrentDateTimeTest.php
+++ tests/Unit/Graph/CurrentDateTimeTest.php
@@ -23,19 +23,24 @@ class CurrentDateTimeTest extends TestCase
     public static function graphFilenameProvider(): array
     {
         return [
-            [__DIR__ . '/../../graph/boesel.png', (new \DateTime())->setTime(9, 3)],
-            [__DIR__ . '/../../graph/friedrichshafen1.png', (new \DateTime())->setTime(10, 0)],
-            [__DIR__ . '/../../graph/friedrichshafen2.png', (new \DateTime())->setTime(20, 58)],
-            [__DIR__ . '/../../graph/hamburg1.png', (new \DateTime())->setTime(17, 50)],
-            [__DIR__ . '/../../graph/hamburg2.png', (new \DateTime())->setTime(9, 54)],
-            [__DIR__ . '/../../graph/lueneburg1.png', (new \DateTime())->setTime(18, 10)],
-            [__DIR__ . '/../../graph/lueneburg2.png', (new \DateTime())->setTime(14, 9)],
-            [__DIR__ . '/../../graph/lueneburg3.png', (new \DateTime())->setTime(15, 9)],
-            [__DIR__ . '/../../graph/lueneburg4.png', (new \DateTime())->setTime(15, 0)],
-            [__DIR__ . '/../../graph/lueneburg5.png', (new \DateTime())->setTime(17, 58)],
-            [__DIR__ . '/../../graph/lueneburg6.png', (new \DateTime())->setTime(11, 9)],
-            [__DIR__ . '/../../graph/schneefernhaus1.png', (new \DateTime())->setTime(9, 50)],
-            [__DIR__ . '/../../graph/schneefernhaus2.png', (new \DateTime())->setTime(15, 10)],
+            [__DIR__ . '/../../graph/boesel.png', self::createDateTime()->setTime(9, 3)],
+            [__DIR__ . '/../../graph/friedrichshafen1.png', self::createDateTime()->setTime(10, 0)],
+            [__DIR__ . '/../../graph/friedrichshafen2.png', self::createDateTime()->setTime(20, 58)],
+            [__DIR__ . '/../../graph/hamburg1.png', self::createDateTime()->setTime(17, 50)],
+            [__DIR__ . '/../../graph/hamburg2.png', self::createDateTime()->setTime(9, 54)],
+            [__DIR__ . '/../../graph/lueneburg1.png', self::createDateTime()->setTime(18, 10)],
+            [__DIR__ . '/../../graph/lueneburg2.png', self::createDateTime()->setTime(14, 9)],
+            [__DIR__ . '/../../graph/lueneburg3.png', self::createDateTime()->setTime(15, 9)],
+            [__DIR__ . '/../../graph/lueneburg4.png', self::createDateTime()->setTime(15, 0)],
+            [__DIR__ . '/../../graph/lueneburg5.png', self::createDateTime()->setTime(17, 58)],
+            [__DIR__ . '/../../graph/lueneburg6.png', self::createDateTime()->setTime(11, 9)],
+            [__DIR__ . '/../../graph/schneefernhaus1.png', self::createDateTime()->setTime(9, 50)],
+            [__DIR__ . '/../../graph/schneefernhaus2.png', self::createDateTime()->setTime(15, 10)],
         ];
+    }
+
+    protected static function createDateTime(): \DateTime
+    {
+        return (new \DateTime())->setTimezone(new \DateTimeZone('Europe/Berlin'));
     }
 }
