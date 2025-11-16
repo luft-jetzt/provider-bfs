003e77db1bb0a07576565afed35d84110bd01e85
Malte Hübner <maltehuebner@gmx.org>
2025-11-16 16:48:00 +0100

    More improvements.

--- src/Bfs/Website/StationLinkExtractor.php
+++ src/Bfs/Website/StationLinkExtractor.php
@@ -7,24 +7,21 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class StationLinkExtractor implements StationLinkExtractorInterface
 {
-    public function __construct()
-    {
-
-    }
-
     public function parseStationLinks(): array
     {
         $htmlContent = $this->loadPageContent();
 
         $crawler = new Crawler($htmlContent);
 
-        $links = $crawler->filter('#main #content ul li a')->each(function (Crawler $node): PageLinkModel
-        {
-            $url = $this->removeSessionId($node->attr('href'));
-            $caption = $node->text();
+        $links = $crawler
+            ->filter('#main #content ul li a')
+            ->each(function (Crawler $node): PageLinkModel {
+                $href = $this->removeSessionId($node->attr('href') ?? '');
+                $url = $this->prefixHostname($href);
+                $caption = $node->text();
 
-            return new PageLinkModel($url, $caption);
-        });
+                return new PageLinkModel($url, $caption);
+            });
 
         return $links;
     }
@@ -40,5 +37,18 @@ class StationLinkExtractor implements StationLinkExtractorInterface
     protected function removeSessionId(string $url): string
     {
         return preg_replace('/;.*$/', '', $url);
+    }
+
+    protected function prefixHostname(string $url): string
+    {
+        if (preg_match('#^https?://#i', $url)) {
+            return $url;
+        }
+
+        if ($url !== '' && $url[0] !== '/') {
+            $url = '/' . $url;
+        }
+
+        return self::HOSTNAME . $url;
     }
 }
